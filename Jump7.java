06b7c9e01f7239a9d1dea4a84cd0cd15f5fd9528
shiyanlou <shiyanlou.163.com>
2021-07-13 19:47:27 +0800

    first commit

---
+++ Jump7.java
@@ -0,0 +1,10 @@
+public class Jump7{
+    public static void main(String[] args){
+        for(int i = 1; i <= 100; i++){
+            if(i%7 == 0 || i%10 == 7 || i/10 == 7){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
